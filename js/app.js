07851882a4ab02401f614077f001c2331e38413c
ChisaeHwang <cwh73090@naver.com>
2022-04-29 04:19:33 +0900

    tag modify

--- js/app.js
+++ js/app.js
@@ -5,6 +5,26 @@ const mainCard = document.querySelector(".main_content > .card");
 const right = document.querySelector(".angle-right");
 const left = document.querySelector(".angle-left");
 const angle = document.querySelector(".angle");
+const Cards = document.querySelectorAll("#Card");
+let count = -1;
+
+function* countNum(i) {
+
+    function* infinity(i = 0) {
+         while (true) yield i++;
+    }
+
+    function* limit(i, num) {
+         for (const a of num) {
+           yield a;
+           if (a == i) return;
+         }
+    }
+
+    for (const a of limit(i.length - 1, infinity())) {
+      yield a;
+    }
+}
 
 TweenMax.to( strClick , 1, {
     autoAlpha: 1,
@@ -43,6 +63,7 @@ mainCard.addEventListener("click", function(){
 right.addEventListener("click", function(){
     document.querySelector(".details").style.transform = "rotateY(-180deg)";
     document.querySelector(".details").style.zIndex = 1;
+
 })
 
 left.addEventListener("click", function(){
