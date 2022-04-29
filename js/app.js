bb7fe3a4e2070395d6a9da38ec8169d5e4521596
wonjun <cwh73090@naver.com>
2022-04-29 15:45:55 +0900

    click evt add

--- js/app.js
+++ js/app.js
@@ -1,24 +1,23 @@
 const strClick = document.querySelector(".STR_content > h3");
 const strblock = document.querySelector(".STR_content");
 const mainEven = document.querySelector(".book");
+const cover = document.querySelector(".cover");
+const pages = document.querySelectorAll(".page");
 const right = document.querySelector(".angle-right");
 const left = document.querySelector(".angle-left");
 const angle = document.querySelector(".angle");
 let count = -1;
 
 function* countNum(i) {
-
     function* infinity(i = 0) {
          while (true) yield i++;
     }
-
     function* limit(i, num) {
          for (const a of num) {
            yield a;
            if (a == i) return;
          }
     }
-
     for (const a of limit(i.length - 1, infinity())) {
       yield a;
     }
@@ -45,12 +44,15 @@ strClick.addEventListener("click", function(){
         delay : Math.random()*1,
         ease:Power3.easeInOut 
     })
-
-    angle.style.opacity = 1;
 })
 
 mainEven.addEventListener("click", function(){
-
+    cover.style.transform = "rotateX(20deg) rotateY(-180deg)";
+    for(const i of countNum(pages)){
+        pages[i].style.transform = "rotateX(20deg) rotateY(-180deg)";
+        pages[i].style.zIndex = 3;
+    }
+    angle.style.opacity = 1;
 })
 
 
